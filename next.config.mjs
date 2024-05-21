53056de023eddf71d17b5a480ad54c5aa7409dac
shalri <fsaonlinework777@gmail.com>
2024-05-21 22:08:10 +0800

    Challenge prep and testing basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // uncomment this when deploying
+  // output: "export",
+  basePath: "/fm-interactive-rating-component",
+  assetPrefix: "/fm-interactive-rating-component",
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        // Primary
+        "irc-orange": "hsl(25, 97%, 53%)",
+        // Neutral
+        "irc-white": "hsl(0, 0%, 100%)",
+        "irc-light-grey": "hsl(217, 12%, 63%)",
+        "irc-dark-blue": "hsl(213, 19%, 18%)",
+        "irc-very-dark-blue": "hsl(216, 12%, 8%)",
+      },
+      fontFamily: {
+        overpass: ["Overpass", "sans-serif"],
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
       },
     },
   },
