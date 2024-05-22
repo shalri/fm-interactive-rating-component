1e1e690d201ffeec57f95746c95e3cb454f029d5
shalri <fsaonlinework777@gmail.com>
2024-05-23 07:42:04 +0800

    Using Suspense wrap

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,9 +1,12 @@
 import ThankYou from "@/components/ThankYou";
+import { Suspense } from "react";
 
 export default function ThankYouPage() {
   return (
     <main className="flex flex-grow  items-center justify-center p-6">
-      <ThankYou />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ThankYou />
+      </Suspense>
     </main>
   );
 }
